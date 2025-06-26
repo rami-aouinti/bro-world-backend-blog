77033c6db1992beaffe3c70402a4322c1164f6ce
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-06-26 04:47:19 +0200

    Add Blog

---
+++ src/Blog/Transport/Controller/Frontend/GetBlogController.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Blog\Transport\Controller\Frontend;
+
+use App\Blog\Domain\Repository\Interfaces\BlogRepositoryInterface;
+use App\General\Domain\Utils\JSON;
+use Closure;
+use Doctrine\ORM\Exception\NotSupported;
+use Exception;
+use JsonException;
+use OpenApi\Attributes as OA;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
+/**
+ * @package App\Blog
+ */
+#[AsController]
+#[OA\Tag(name: 'Blog')]
+readonly class GetBlogController
+{
+    public function __construct(
+        private SerializerInterface $serializer,
+        private CacheInterface $cache,
+        private BlogRepositoryInterface $blogRepository
+    ) {
+    }
+
+    /**
+     * Get current user blog data, accessible only for 'IS_AUTHENTICATED_FULLY' users
+     *
+     * @param string $slug
+     *
+     * @throws ExceptionInterface
+     * @throws InvalidArgumentException
+     * @throws JsonException
+     * @return JsonResponse
+     */
+    #[Route(path: '/platform/blog/{slug}', name: 'public_blog_slug', methods: [Request::METHOD_GET])]
+    public function __invoke(string $slug): JsonResponse
+    {
+        $cacheKey = 'public_blog_' . $slug;
+        $blogs = $this->cache->get($cacheKey, fn (ItemInterface $item) => $this->getClosure($slug)($item));
+        $output = JSON::decode(
+            $this->serializer->serialize(
+                $blogs,
+                'json',
+                [
+                    'groups' => 'Blog',
+                ]
+            ),
+            true,
+        );
+        return new JsonResponse($output);
+    }
+
+    /**
+     *
+     * @param string $slug
+     *
+     * @return Closure
+     */
+    private function getClosure(string $slug): Closure
+    {
+        return function (ItemInterface $item) use ($slug): array {
+            $item->expiresAfter(3600);
+
+            return $this->getFormattedPosts($slug);
+        };
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function getFormattedPosts(string $slug): array
+    {
+        return $this->getBlog($slug);
+    }
+
+    /**
+     * @param $slug
+     *
+     * @throws NotSupported
+     * @return array
+     */
+    private function getBlog($slug): array
+    {
+        return $this->blogRepository->findOneBy([
+            'slug' => $slug
+        ]);
+    }
+}
