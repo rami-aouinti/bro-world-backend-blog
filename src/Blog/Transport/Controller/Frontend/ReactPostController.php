f122afbf1a5dd8f665bbdd774d41e1b45b52fd3f
RamiAouinti <rami.aouinti@tkdeutschland.de>
2025-08-05 01:57:07 +0200

    add Reactions

--- src/Blog/Transport/Controller/Frontend/ReactPostController.php
+++ src/Blog/Transport/Controller/Frontend/ReactPostController.php
@@ -10,11 +10,11 @@ use App\Blog\Domain\Message\CreateNotificationMessenger;
 use App\Blog\Domain\Repository\Interfaces\ReactionRepositoryInterface;
 use App\General\Domain\Utils\JSON;
 use App\General\Infrastructure\ValueObject\SymfonyUser;
+use Doctrine\ORM\Exception\NotSupported;
 use Doctrine\ORM\Exception\ORMException;
 use Doctrine\ORM\OptimisticLockException;
 use JsonException;
 use OpenApi\Attributes as OA;
-use Psr\Cache\InvalidArgumentException;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,7 +25,10 @@ use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 /**
- * @package App\Blog
+ * Class ReactPostController
+ *
+ * @package App\Blog\Transport\Controller\Frontend
+ * @author  Rami Aouinti <rami.aouinti@tkdeutschland.de>
  */
 #[AsController]
 #[OA\Tag(name: 'Blog')]
@@ -35,11 +38,10 @@ readonly class ReactPostController
         private SerializerInterface $serializer,
         private ReactionRepositoryInterface $reactionRepository,
         private MessageBusInterface $bus
-    ) {
-    }
+    ) {}
 
     /**
-     * Get current user blog data, accessible only for 'IS_AUTHENTICATED_FULLY' users.
+     * Handles user reactions on a post (add, update, delete).
      *
      * @param SymfonyUser $symfonyUser
      * @param Request     $request
@@ -48,77 +50,95 @@ readonly class ReactPostController
      *
      * @throws ExceptionInterface
      * @throws JsonException
+     * @throws NotSupported
      * @throws ORMException
      * @throws OptimisticLockException
      * @throws \Symfony\Component\Messenger\Exception\ExceptionInterface
-     * @throws InvalidArgumentException
      * @return JsonResponse
      */
     #[Route(path: '/v1/private/post/{post}/react/{type}', name: 'react_post', methods: [Request::METHOD_POST])]
     public function __invoke(SymfonyUser $symfonyUser, Request $request, Post $post, string $type): JsonResponse
     {
-        $reaction = $this->reactionRepository->findOneBy([
-            'user' => Uuid::fromString($symfonyUser->getUserIdentifier()),
-            'post' => $post->getId()
-        ]);
-        if ($type === 'delete') {
-            if ($reaction) {
-                $this->reactionRepository->remove($reaction);
-                $output = JSON::decode(
-                    $this->serializer->serialize(
-                        'success',
-                        'json',
-                        [
-                            'groups' => 'Reaction',
-                        ]
-                    ),
-                    true,
-                );
-            } else {
-                $output = JSON::decode(
-                    $this->serializer->serialize(
-                        'reaction not found',
-                        'json',
-                        [
-                            'groups' => 'Reaction',
-                        ]
-                    ),
-                    true,
-                );
-            }
-        } else {
-            if ($reaction) {
-                $this->reactionRepository->remove($reaction);
-            }
-            $reaction = new Reaction();
-            $reaction->setPost($post);
-            $reaction->setUser(Uuid::fromString($symfonyUser->getUserIdentifier()));
-            $reaction->setType($type);
-            $this->bus->dispatch(
-                new CreateNotificationMessenger(
-                    $request->headers->get('Authorization'),
-                    'PUSH',
-                    $symfonyUser->getUserIdentifier(),
-                    $post->getAuthor()->toString(),
-                    $post->getId(),
-                    'reacted to your post.'
-                )
-            );
-            $this->reactionRepository->save($reaction);
-            $result = [];
-            $result['id'] = $reaction->getId();
-            $result['user'] = $symfonyUser;
-            $output = JSON::decode(
-                $this->serializer->serialize(
-                    $result,
-                    'json',
-                    [
-                        'groups' => 'Reaction',
-                    ]
-                ),
-                true,
-            );
+        $reaction = $this->reactionRepository->findOneBy(['user' => Uuid::fromString($symfonyUser->getUserIdentifier()), 'post' => $post->getId()]);
+
+        return $type === 'delete'
+            ? $this->handleDeleteReaction($reaction)
+            : $this->handleAddOrUpdateReaction($reaction, $post, $symfonyUser, $request);
+    }
+
+    /**
+     *
+     * @param Reaction|null $reaction
+     *
+     * @throws ExceptionInterface
+     * @throws JsonException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     * @return JsonResponse
+     */
+    private function handleDeleteReaction(?Reaction $reaction): JsonResponse
+    {
+        if ($reaction) {
+            $this->reactionRepository->remove($reaction);
+            return $this->jsonResponse('success');
         }
-        return new JsonResponse($output);
+
+        return $this->jsonResponse('reaction not found');
+    }
+
+    /**
+     *
+     * @param Reaction|null $existingReaction
+     * @param Post          $post
+     * @param SymfonyUser   $symfonyUser
+     * @param Request       $request
+     *
+     * @throws ExceptionInterface
+     * @throws JsonException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     * @throws \Symfony\Component\Messenger\Exception\ExceptionInterface
+     * @return JsonResponse
+     */
+    private function handleAddOrUpdateReaction(?Reaction $existingReaction, Post $post, SymfonyUser $symfonyUser, Request $request): JsonResponse
+    {
+        if ($existingReaction) {
+            $this->reactionRepository->remove($existingReaction);
+        }
+
+        $reaction = new Reaction();
+        $reaction->setPost($post);
+        $reaction->setUser(Uuid::fromString($symfonyUser->getUserIdentifier()));
+        $reaction->setType($request->attributes->get('type'));
+
+        $this->bus->dispatch(
+            new CreateNotificationMessenger(
+                $request->headers->get('Authorization'),
+                'PUSH',
+                $symfonyUser->getUserIdentifier(),
+                $post->getAuthor()->toString(),
+                $post->getId(),
+                'reacted to your post.'
+            )
+        );
+
+        $this->reactionRepository->save($reaction);
+
+        return $this->jsonResponse([
+            'id' => $reaction->getId(),
+            'user' => $symfonyUser
+        ]);
+    }
+
+    /**
+     *
+     * @param mixed $data
+     * @return JsonResponse
+     * @throws ExceptionInterface|JsonException
+     */
+    private function jsonResponse(mixed $data): JsonResponse
+    {
+        $json = JSON::decode($this->serializer->serialize($data, 'json', ['groups' => 'Reaction']), true);
+        return new JsonResponse($json);
     }
 }
